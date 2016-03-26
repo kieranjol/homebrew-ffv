3aeeec031373bdaebbdd2619fc3ab753345c7901
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-03-26 10:07:51 +0000

    ifiscripts - python fix?

--- ifiscripts.rb
+++ ifiscripts.rb
@@ -1,12 +1,9 @@
-
-
 class Ifiscripts < Formula
   desc ""
   homepage "https://github.com/kieranjol/IFIscripts"
   url "https://github.com/kieranjol/IFIscripts/archive/v0.1-alpha.tar.gz"
   version "alpha"
   sha256 "ae2559b63f50a350532961de031757e3104c880c732155d380034dde4bfb351e"
-
 
   depends_on 'cowsay'
   depends_on 'dvdauthor'
@@ -15,17 +12,8 @@ class Ifiscripts < Formula
   depends_on 'mediainfo'
   depends_on 'xmlstarlet'
   
-
   def install
-    bin.install "batchffv1.py"
-    bin.install "bitc.py"
-    bin.install "ffv1trans.py"
-    bin.install "ififuncs.py"
-    bin.install "libimages.py"
-    bin.install "makeffv1.py"
-    bin.install "revtmd.py"
-
+    libexec.install "batchffv1.py", "bitc.py", "ffv1trans.py", "ififuncs.py", "libimages.py", "makeffv1.py", "revtmd.py"
   end
-
   
 end
